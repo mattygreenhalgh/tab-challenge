fe5b05cc78d719360b75de0085dd2eedf51d0d22
mattygreenhalgh <62141308+mattygreenhalgh@users.noreply.github.com>
2020-03-13 13:55:12 +0000

    added comments
    addded commets to the code

--- Stage1/tabs.js
+++ Stage1/tabs.js
@@ -6,12 +6,15 @@ tabs.forEach(tab => {
     tab.addEventListener('click', () => {
         const target = document.querySelector(tab.dataset.tabTarget)
         tabContents.forEach(tabContent => {
-            tabContent.classList.remove('active')
-        })
+                tabContent.classList.remove('active')
+            })
+            //removes active class 
         tabs.forEach(tab => {
-            tab.classList.remove('active')
-        })
+                tab.classList.remove('active')
+            })
+            //adds active class to the tab that is active
         tab.classList.add('active')
+            //adds active class to the content that is active
         target.classList.add('active')
     })
 });

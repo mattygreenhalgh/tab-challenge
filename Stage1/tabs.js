466e2fe339bc07f44529c4374918fe2f890950d2
mattygreenhalgh <62141308+mattygreenhalgh@users.noreply.github.com>
2020-03-13 12:58:32 +0000

    creating loop
    Creating a loop that will loop through the tabs when clicked

--- Stage1/tabs.js
+++ Stage1/tabs.js
@@ -1,1 +1,9 @@
 console.log('log from tabs.js');
+const tabs = querySelectorAll('[data-tab-target]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = querySelector(tab.dataset.tabTarget)
+        target.classlist.add('active')
+    })
+});
